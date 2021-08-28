4aca68fe2357abb7adb4bddaef76d78da6c3f39e
Ryo Ando <sasigume@gmail.com>
2021-08-29 01:09:13 +0900

    更新の関数を忘れていたので修正

--- functions/src/apiv4_appv2/index.ts
+++ functions/src/apiv4_appv2/index.ts
@@ -1,3 +1,5 @@
 export { default as getConverted } from './getConverted';
 export { default as getByIdConverted } from './getByIdConverted';
 export { default as getRss } from './getRss';
+export { default as updateFirestore } from './updateFirestore';
+export { default as updateFirestoreManually } from './updateFirestoreManually';
