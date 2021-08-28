1c419ec4f533831404b6d7b263f812af76b498f1
Ryo Ando <sasigume@gmail.com>
2021-08-29 02:32:35 +0900

    lintがwindow.gtag消しやがった

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,18 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "rss";
-
-// https://sunday-morning.app/posts/2020-12-09-nextjs-google-analytics
-interface Window {
-  gtag(type: "config", googleAnalyticsId: string, { page_path: string });
-  gtag(
-    type: "event",
-    eventAction: string,
-    fieldObject: {
-      event_label: string;
-      event_category: string;
-      value?: number;
-    }
-  );
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
