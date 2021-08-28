f90ff78bf60797305be037d215c5978e113ca897
Ryo Ando <sasigume@gmail.com>
2021-08-29 01:11:13 +0900

    バックアップ先のコレクションを間違えていたので修正

--- functions/src/apiv4_appv2/common/collections.ts
+++ functions/src/apiv4_appv2/common/collections.ts
@@ -2,4 +2,4 @@ import * as admin from 'firebase-admin';
 export const COLLECTION_APIV4_APPV2 = admin.firestore().collection('animeCollection_apiv4_appv2');
 export const COLLECTION_APIV4_APPV2_BACKUP = admin
   .firestore()
-  .collection('animeCollection_apiv4_appv2');
+  .collection('animeCollection_apiv4_appv2_backup');
