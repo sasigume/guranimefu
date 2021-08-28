66803c42254e0984f7ad9dd87ceb0d5f3a696b29
Ryo Ando <sasigume@gmail.com>
2021-08-29 03:01:19 +0900

    gtag残っていたので削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,19 @@
-import { useEffect } from 'react'
-import { AppProps } from 'next/app'
-import dayjs from 'dayjs'
-import 'dayjs/locale/ja'
-dayjs.locale('ja')
+import { AppProps } from 'next/app';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
+dayjs.locale('ja');
 
-import * as gtag from '@/lib/gtag'
-import { Chakra } from '@/components/providers/chakra'
-import { useRouter } from 'next/router'
-
+import { Chakra } from '@/components/providers/chakra';
 
 function App({ Component, pageProps }: AppProps) {
-
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
   return (
     <Chakra cookies={pageProps.cookies}>
       <Component {...pageProps} />
     </Chakra>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // https://chakra-ui.com/docs/features/color-mode
-export { getServerSideProps } from "@/components/providers/chakra"
+export { getServerSideProps } from '@/components/providers/chakra';
