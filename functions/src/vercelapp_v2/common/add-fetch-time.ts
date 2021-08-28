057dd5775e4d1bf61edb74bb62ba1c0c2141313d
Ryo Ando <sasigume@gmail.com>
2021-08-29 00:33:13 +0900

    v2APIを削除

--- functions/src/vercelapp_v2/common/add-fetch-time.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-import { AnimeForGraph, AnimeForGraphWithLastFetched } from '../../models/mal_v2';
-
-export const addFetchTime = (anime: AnimeForGraph): AnimeForGraphWithLastFetched => {
-  return {
-    ...anime,
-    lastFetched: admin.firestore.Timestamp.now(),
-  };
-};
