5e4f8edaa19302303cb1e2135d7944000ef7f01a
Ryo Ando <sasigume@gmail.com>
2021-08-30 02:01:36 +0900

    ロゴを追加

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react';
+
+const Logo = () => {
+  return (
+    <Box>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fillRule="evenodd"
+        strokeLinecap="square"
+        strokeMiterlimit="100"
+        clipRule="evenodd"
+        viewBox="0 0 382 112"
+      >
+        <g fill="none" stroke="#000" strokeWidth="9" clipPath="url(#a)">
+          <path d="M8 55l20-30 40 1-16 61H26M136 25h27M119 42h51l-14 45h-33M200 25h31M182 87h60M264 45l38 19M299 25l-32 62M322 25h52l-34 62M88 25l-5 26M105 25l-4 26" />
+        </g>
+      </svg>
+    </Box>
+  );
+};
+
+export default Logo;
--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,12 +1,27 @@
-import { SITE_DESC, SITE_NAME } from '@/lib/constants'
-import { Flex, Heading, Box } from '@chakra-ui/react'
-import LinkChakra from '../common/link-chakra'
+import { SITE_DESC, SITE_NAME } from '@/lib/constants';
+import { Flex, Heading, Box } from '@chakra-ui/react';
+import LinkChakra from '../common/link-chakra';
+import Logo from '../common/logo';
 
 export const Hero = () => (
-  <Flex border="none" borderBottom="solid" borderWidth={2} borderColor="gray.400" direction={{ base: "column", md: "row" }} justifyContent="space-between" alignItems="center" pt={12} pb={8} mb={4}>
-    <Heading as="h1" mr={4} fontSize={{ base: "3rem", md: "2rem" }}>
-      <LinkChakra href="/">{SITE_NAME}</LinkChakra>
-    </Heading>
+  <Flex
+    border="none"
+    borderBottom="solid"
+    borderWidth={2}
+    borderColor="gray.400"
+    direction={{ base: 'column', md: 'row' }}
+    justifyContent="space-between"
+    alignItems="center"
+    pt={12}
+    pb={8}
+    mb={4}
+    gridGap={3}
+  >
+    <Box>
+      <Logo />
+      <Heading as="h1">GURANIMEFU</Heading>
+    </Box>
+
     <Box fontSize="1.4rem">{SITE_DESC}</Box>
   </Flex>
-)
+);
